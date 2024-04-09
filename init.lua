8b125135fe44d71453e2b44b7dbc737fece7a86e
Manos Koumandakis <manykoum@msn.com>
2024-04-09 20:47:53 +0200

    removed `indent_blankline` plugin

--- init.lua
+++ init.lua
@@ -263,15 +263,15 @@ require('lazy').setup({
     },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+  -- { -- Add indentation guides even on blank lines
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   -- Enable `lukas-reineke/indent-blankline.nvim`
+  --   -- See `:help indent_blankline.txt`
+  --   opts = {
+  --     char = '┊',
+  --     show_trailing_blankline_indent = false,
+  --   },
+  -- },
 
   { -- Cheatsheet for nvim keybindings
     'sudormrfbin/cheatsheet.nvim',

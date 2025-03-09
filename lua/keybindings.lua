ea06a598d4edd8d27614d704422e197af14776de
Manos Koumandakis <manykoum@msn.com>
2025-03-09 16:38:36 +0100

    small changes in keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,2 +1,5 @@
-# exit insert mode with jj
+-- set the vertical bar at 80 characters
+vim.cmd 'set colorcolumn=80'
+
+-- exit insert mode with jj
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true })
